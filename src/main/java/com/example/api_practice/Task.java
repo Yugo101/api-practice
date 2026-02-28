6169de6063e26361a0c6cc2d19791994f76d65d7
BZS_Higashiyama <bzg.yugo.higasiyama@gmail.com>
2026-02-28 15:50:02 +0900

    Implement POST /tasks API with @RequestBody and GET list endpoint

--- src/main/java/com/example/api_practice/Task.java
+++ src/main/java/com/example/api_practice/Task.java
@@ -13,6 +13,8 @@ public class Task {
 
     private String title;
 
+    private boolean completed;
+
     public Task(){}
 
     public Task(String title){
@@ -27,7 +29,15 @@ public class Task {
         return title;
     }
 
+    public boolean isCompleted() {
+        return completed;
+    }
+
     public void setTitle(String title){
         this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
